4db653a740b5c042189b714b5122d335a5a2c085
unknown <russellleininger@u.boisestate.edu>
2019-02-16 15:59:49 -0700

    Skeleton FAQ

--- faq.php
+++ faq.php
@@ -2,6 +2,12 @@
 $thisPage = "faq";
 include_once("header.php");
 ?>
-<h1>FAQ</h1>
+<h1>Frequently Asked Questions</h1>
+<ul id="faq">
+    <li class="question">What is love?</li>
+    <li class="answer">Baby don't hurt me!</li>
+    <li class="question">What is the meaning of life?</li>
+    <li class="answer">42</li>
+</ul>
 <?php
 include_once ("footer.html");
